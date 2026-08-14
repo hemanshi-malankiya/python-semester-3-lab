4158fa90a1ab40546963634d1a2e0393263c407e
Hemanshi Malankiya <hemanshimalankiya@gmail.com>
2026-08-14 23:22:27 +0530

    73.

--- journal.py
+++ journal.py
@@ -1026,6 +1026,36 @@ if len(list2) == len(set(list2)):
 else:
     print("Is the said list contains all unique elements!")
     print(False)
+##
+##73. Write a Python program to sort a list of lists by a
+##given index of the inner list. 
+##Original list:
+##[('Greyson Fulton', 98, 99), ('Brady Kent', 97, 96),
+## ('Wyatt Knott', 91, 94), ('Beau Turnbull', 94, 98)]
+##Sort the said list of lists by a given index ( Index = 0 ) of the inner list
+##[('Beau Turnbull', 94, 98), ('Brady Kent', 97, 96),
+## ('Greyson Fulton', 98, 99), ('Wyatt Knott', 91, 94)]
+##Sort the said list of lists by a given index ( Index = 2 ) of the inner list
+##[('Wyatt Knott', 91, 94), ('Brady Kent', 97, 96),
+## ('Beau Turnbull', 94, 98), ('Greyson Fulton', 98, 99)]
+
+data = [
+    ('Greyson Fulton', 98, 99),
+    ('Brady Kent', 97, 96),
+    ('Wyatt Knott', 91, 94),
+    ('Beau Turnbull', 94, 98)
+]
+
+print("Original list:")
+print(data)
+
+print("\nSort by index 0:")
+print(sorted(data, key=lambda x: x[0]))
+
+print("\nSort by index 2:")
+print(sorted(data, key=lambda x: x[2]))    
+
+    
 
 
 
