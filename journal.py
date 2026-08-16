b2c0f18f49e9bc063e38a181eb76c2d2d341cee4
Hemanshi Malankiya <hemanshimalankiya@gmail.com>
2026-08-16 22:57:55 +0530

    80

--- journal.py
+++ journal.py
@@ -1211,6 +1211,35 @@ minimum = min(products)
 print("Maximum and minimum product from the pairs of the said tuple of list:")
 print((maximum, minimum))
 
+##80. Write a Python program to interleave multiple lists of the same length. 
+##Original list:
+##list1: [1, 2, 3, 4, 5, 6, 7]
+##list2: [10, 20, 30, 40, 50, 60, 70]
+##list3: [100, 200, 300, 400, 500, 600, 700]
+##Interleave multiple lists:
+##[1, 10, 100, 2, 20, 200, 3, 30, 300, 4, 40, 400, 5, 50, 500,
+## 6, 60, 600, 7, 70, 700]
+list1 = [1, 2, 3, 4, 5, 6, 7]
+list2 = [10, 20, 30, 40, 50, 60, 70]
+list3 = [100, 200, 300, 400, 500, 600, 700]
+
+print("Original list:")
+print("list1:", list1)
+print("list2:", list2)
+print("list3:", list3)
+
+result = []
+
+for i in range(len(list1)):
+    result.append(list1[i])
+    result.append(list2[i])
+    result.append(list3[i])
+
+print("Interleave multiple lists:")
+print(result)
+
+
+
 
     
 
