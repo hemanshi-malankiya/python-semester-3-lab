36e37bc0389dd38062ecc7e808aca4ae157189d4
Hemanshi Malankiya <hemanshimalankiya@gmail.com>
2026-08-17 23:00:17 +0530

    83

--- journal.py
+++ journal.py
@@ -1325,7 +1325,29 @@ print(l)
 print("Sum of the specified range:")
 print(total)
 
+##83. Write a Python program to reverse each list in a given list of lists. 
+##Original list of lists:
+##[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+##Reverse each list in the said list of lists:
+##[[4, 3, 2, 1], [8, 7, 6, 5], [12, 11, 10, 9], [16, 15, 14, 13]]
+##
 
+
+l = [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+     [13, 14, 15, 16]]
+
+result = []
+
+for x in l:
+    result.append(x[::-1])
+
+print("Original list of lists:")
+print(l)
+
+print("Reverse each list:")
+print(result)
 
     
 
