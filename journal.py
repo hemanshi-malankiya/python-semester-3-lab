fe2393d0b9396765643fc6c7f5df8993e48a3db7
Hemanshi Malankiya <hemanshimalankiya@gmail.com>
2026-08-19 23:17:54 +0530

    89

--- journal.py
+++ journal.py
@@ -1473,7 +1473,28 @@ for i in range(len(l)):
         print("Index of the first element which is greater than", number, ":")
         print(i)
         break
+    
+##89. Write a Python program to split a given list into specified sized chunks. 
+##Original list:
+##[12, 45, 23, 67, 78, 90, 45, 32, 100, 76, 38, 62, 73, 29, 83]
+##Split the said list into equal size 3
+##[[12, 45, 23], [67, 78, 90], [45, 32, 100], [76, 38, 62], [73, 29, 83]]
+##Split the said list into equal size 4
+##[[12, 45, 23, 67], [78, 90, 45, 32], [100, 76, 38, 62], [73, 29, 83]]
+##Split the said list into equal size 5
+##[[12, 45, 23, 67, 78], [90, 45, 32, 100, 76], [38, 62, 73, 29, 83]]
+l = [12, 45, 23, 67, 78, 90, 45, 32, 100, 76, 38, 62, 73, 29, 83]
 
+sizes = [3, 4, 5]
+
+for size in sizes:
+    result = []
+
+    for i in range(0, len(l), size):
+        result.append(l[i:i + size])
+
+    print("Split into size", size, ":")
+    print(result)
 
 
 
